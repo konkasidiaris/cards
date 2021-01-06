9ba9a3677693fcf660504227f445769316f071ce
Konstantinos <kkasidiaris@gmail.com>
2021-01-06 03:41:29 +0200

    created a cards project and added some methods

---
+++ lib/cards.ex
@@ -0,0 +1,50 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  def create_deck do
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
+    suits = ["Spades", "Diamonds", "Hearts", "Clubs"]
+
+    mixSuitsAndValues(suits, values)
+  end
+
+  defp mixSuitsAndValues(suits, values) do
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+end
